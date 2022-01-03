e64d4cbcaceaf823d6532c630200568cc4d87288
Ana Burtet <analiseburtet@gmail.com>
2022-01-03 20:15:08 -0300

    add toggleStatus and removeTask feature

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,17 +2,28 @@ import React from 'react'
 import { Typography, Checkbox, IconButton } from "@mui/material";
 import { Task } from './types';
 import { DeleteForever } from '@mui/icons-material';
+import {useTodoList} from "../hooks/useTodoList";
 
 interface TaskCardProps {
     task: Task
 }
 
-export const TaskCard = ({ task: { done, text } }: TaskCardProps) => {
+export const TaskCard = ({ task: { done, text, id } }: TaskCardProps) => {
+    const { removeTask, toggleStatus } = useTodoList()
+    const handleClick = () => removeTask(id)
+    const handleToggle = () => toggleStatus(id)
+
     return (
         <>
-            <Checkbox checked={done} />
+            <Checkbox
+                checked={done}
+                onClick={handleToggle}
+            />
             <Typography>{text}</Typography>
-            <IconButton aria-label="settings">
+            <IconButton
+                aria-label="settings"
+                onClick={handleClick}
+            >
                 <DeleteForever />
             </IconButton>
         </>
--- src/provider/TodoListProvider.tsx
+++ src/provider/TodoListProvider.tsx
@@ -4,20 +4,31 @@ import {Task} from "../components/types";
 type TodoListContextProps = {
     list: Task[]
     addTask: (text:string) => void
-    removeTask: () => void
+    removeTask: (id:number) => void
+    toggleStatus: (id:number) => void
 }
 
 export const TodoListContext = createContext({} as TodoListContextProps);
 
 const TodoListProvider:FC = ({children}) => {
     const [list,setList] = useState<Task[]>([])
+
     const addTask = (text:string) => {
         setList([...list, {text, id: Math.random(), done: false}])
     }
-    const removeTask = () => {}
+
+    const removeTask = (id:number) => {
+        const newList = list.filter((list) => list.id !== id)
+        setList(newList)
+    }
+
+    const toggleStatus = (id:number) => {
+        const newList = list.map((task) => task.id === id ? {...task, done: !task.done } : task)
+        setList(newList)
+    }
 
     return (
-        <TodoListContext.Provider value={{list, addTask, removeTask}}>
+        <TodoListContext.Provider value={{list, addTask, removeTask, toggleStatus}}>
             {children}
         </TodoListContext.Provider>
     )
