0f76d51e6f79a66398b62c62399ee04aea26e180
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-03 19:13:21 -0300

    removendo imports inuteis

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next'
 import Header from "../components/Header";
 import AddTask from "../components/AddTask";
-import TaskCard from "../components/TaskCard";
 import { Task } from '../components/types';
 import ListCards from '../components/ListCards';
 
 const Home: NextPage = () => {
 
   const tasks: Task[] = "____".split("")
-    .map((_) => ({
+    .map((_, index) => ({
       done: true,
-      text: "task"
+      text: "task",
+      id: index
     }))
 
   return (
